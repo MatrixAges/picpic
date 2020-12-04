26ac0c01545468232c5e5520cd406844c00578ba
MatrixAge <xiewendao123@foxmail.com>
2020-12-04 12:36:10 +0800

    [_]

--- build/index.js
+++ build/index.js
@@ -1,8 +1,17 @@
-const fs = require('fs')
+const fs = require('fs-extra')
 const globby = require('globby')
 
 const main = async () => {
-	const paths = await globby([ '*.png', '*.svg', '*.jpg', '*.jpeg' ])
+	const paths_source = await globby([
+		'assets/*.png',
+		'assets/*.svg',
+		'assets/*.jpg',
+		'assets/*.jpeg'
+	])
+      const paths_target = []
+      
+	paths_source.map(item => paths_target.push(item.substr(7)))
+
 	const html = fs.readFileSync('src/source.html').toString().replace(
 		`
       <head>
@@ -13,15 +22,18 @@ const main = async () => {
       <head>
             <title>MatirxAge images bed</title>
             <script>
-                  window.img_paths=${JSON.stringify(paths)}
+                  window.img_paths=${JSON.stringify(paths_target)}
             </script>
       </head>
 `
-      )
-      
-      if (!fs.existsSync('dist')) fs.mkdirSync('dist')
-      
-      fs.writeFileSync('dist/index.html', html)
+	)
+
+	if (!fs.existsSync('dist')) fs.mkdirSync('dist')
+
+	fs.writeFileSync('dist/index.html', html)
+	fs.copySync('assets', 'dist')
+	fs.copySync('src', 'dist')
+	fs.removeSync('dist/source.html')
 }
 
 main()
